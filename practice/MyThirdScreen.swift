112791b60c034102403cfbf8eaf9a14a41bc6a46
pyshivam <pyshivam.py@gmail.com>
2018-11-05 16:04:09 +0530

    test

--- practice/MyThirdScreen.swift
+++ practice/MyThirdScreen.swift
@@ -13,7 +13,7 @@ class MyThirdScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print("View is loaded")
     }
     
 
