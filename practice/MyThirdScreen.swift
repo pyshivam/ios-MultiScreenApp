998468ec2a0ce9644b3ab2d4338e8f4de8d315be
pyshivam <pyshivam.py@gmail.com>
2018-11-05 15:58:49 +0530

    Initial Commit

---
+++ practice/MyThirdScreen.swift
@@ -0,0 +1,30 @@
+//
+//  MyThirdScreen.swift
+//  practice
+//
+//  Created by Shivam Gupta on 11/4/18.
+//  Copyright © 2018 Shivam Gupta. All rights reserved.
+//
+
+import UIKit
+
+class MyThirdScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by Shivam Gupta on 11/4/18.
+//  Copyright © 2018 Shivam Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
